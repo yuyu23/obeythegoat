33fd56c1d55b7a0a4a93e290d674e88a998d6adc
Cheyenne Swarthout <Cheyenne Swarthout>
2016-10-12 13:57:49 -0400

    Basic view no returns minimal HTML

--- functional_tests.py
+++ functional_tests.py
@@ -3,21 +3,21 @@ import unittest
 
 class NewVisitorTest(unittest.TestCase):  
 
-    def setUp(self):  
-        self.browser = webdriver.Firefox()
+	def setUp(self):
+		self.browser = webdriver.Firefox()
 		self.browser.implicitly_wait(3)
 
-    def tearDown(self):  
-        self.browser.quit()
+	def tearDown(self):
+		self.browser.quit()
 
-    def test_can_start_a_list_and_retrieve_it_later(self):  
-        # Edith has heard about a cool new online to-do app. She goes
-        # to check out its homepage
-        self.browser.get('http://localhost:8000')
-
-        # She notices the page title and header mention to-do lists
-        self.assertIn('To-Do', self.browser.title)  
-        self.fail('Finish the test!')
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		# Edith has heard about a cool new online to-do app. She goes
+		# to check out its homepage
+		self.browser.get('http://localhost:8000')
+		
+		# She notices the page title and header mention to-do lists
+		self.assertIn('To-Do', self.browser.title)
+		self.fail('Finish the test!')
 
 # She is invited to enter a to-do item straight away
 
@@ -40,5 +40,5 @@ class NewVisitorTest(unittest.TestCase):
 
 # Satisfied, she goes back to sleep
 
-if __name__ == '__main__':  
-    unittest.main(warnings='ignore')
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')
