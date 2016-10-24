30285d7e511db00a5715e4e73f6be6fe6ffb754e
Cheyenne Swarthout <Cheyenne Swarthout>
2016-10-24 17:41:18 -0400

    Added blank=True and null=True to models.py

--- lists/models.py
+++ lists/models.py
@@ -5,4 +5,9 @@ class List(models.Model):
 
 class Item(models.Model):
 	text = models.TextField(default='')
-	list = models.ForeignKey(List, default=None)
+	list = models.ForeignKey(
+		List,
+		default=None,
+		blank=True,
+		null=True,
+	)
