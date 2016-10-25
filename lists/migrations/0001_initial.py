713190844a31e08be68d29852f552f8a6173a6db
Cheyenne Swarthout <Cheyenne Swarthout>
2016-10-25 17:46:07 -0400

    removed migrations and redid makemigrations

--- lists/migrations/0001_initial.py
+++ lists/migrations/0001_initial.py
@@ -13,7 +13,19 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Item',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', auto_created=True, serialize=False, primary_key=True)),
+                ('id', models.AutoField(auto_created=True, serialize=False, primary_key=True, verbose_name='ID')),
+                ('text', models.TextField(default='')),
             ],
+        ),
+        migrations.CreateModel(
+            name='List',
+            fields=[
+                ('id', models.AutoField(auto_created=True, serialize=False, primary_key=True, verbose_name='ID')),
+            ],
+        ),
+        migrations.AddField(
+            model_name='item',
+            name='list',
+            field=models.ForeignKey(default=None, to='lists.List', null=True),
         ),
     ]
