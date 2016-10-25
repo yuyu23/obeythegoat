3f71b36bab56b922107afcf2aafc55c2542ba926
yuyu23 <yutikohercell@gmail.com>
2016-10-25 18:00:53 -0400

    Added blank=True to file

--- lists/models.py
+++ lists/models.py
@@ -6,4 +6,4 @@ class List(models.Model):
 
 class Item(models.Model):
 	text = models.TextField(default='')
-	list = models.ForeignKey(List, default=None, null=True)
+	list = models.ForeignKey(List, default=None, null=True, blank=True)
